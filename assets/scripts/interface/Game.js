3f7109de7fb85f4c94d4289c7627f18a11786221
席美娜 <15701376183@189.cn>
2020-02-27 16:09:20 +0800

    prefab封装 2020/2/27

--- assets/scripts/interface/Game.js
+++ assets/scripts/interface/Game.js
@@ -6,23 +6,19 @@ var engine = require('../MatchvsLib/MatchvsEngine');
 var response = require("../MatchvsLib/MatchvsResponse");
 var msg = require("../MatchvsLib/MatvhsvsMessage");
 var GameData = require('../MatchvsLib/ExamplesData');
+// var Player = require()
 
 cc.Class({
     extends: cc.Component,
 
     properties: {
+        player_prefab: {
+            type: cc.Prefab,
+            default: null,
+        },
+        autoLoad: true,   //自动加载
         giveFood:cc.Node,
-        players: [cc.Node],
-
-        scoreDisplays0: {
-            default: null,
-            type: cc.Label
-        },
-        scoreDisplays1: {
-            default: null,
-            type: cc.Label
-        },
-
+        players:[],
         useridLabel: {
             default: null,
             type: cc.Label
@@ -33,17 +29,50 @@ cc.Class({
         },
         score:0,
         userScores:[],
+        myScore:"",
         sendPos:"",
     },
 
 
     onLoad: function () {
-        console.log(GameData.totolUser);
+        //加载预制体
+        if (this.autoLoad) {
+            for(let i=0;i<2;i++){
+                this.loadPrefab();
+            }
+            let Chlidren = this.node.children;
+            for (let i=0;i<Chlidren.length;i++){
+                if(Chlidren[i].name == 'Player'){
+                    this.players.push(Chlidren[i])
+                    // console.log(Chlidren[i]);
+                }
+            }
+
+        }
+
+        ///控制角色移动
+        this.node.on('touchstart', function (event) {
+            var pos = event.getLocation();
+            pos = this.node.convertToNodeSpaceAR(pos);
+            for (let i = 0; i < this.players.length; i++) {
+                var lableId = this.players[i].getChildByName("AnimNode").getChildByName("playerLabel").getComponent(cc.Label).string;
+                console.log(lableId)
+                 console.log(GameData.userID)
+                if(lableId == "我"){
+                    if(pos.y > -180 && pos.y < 140){
+                        this.players[i].getComponent("Player").movePlayer(this.players[i],pos);
+                    }
+
+                }
+
+            }
+        }, this);
+
+        console.log(GameData);
         this.initMatchvsEvent(this);
 
         this.useridLabel.string = "用户Id:" + GameData.userID;
-        console.log(this.players[1].getChildByName("AnimNode").getChildByName("playerLabel").getComponent(cc.Label).string)
-        // console.log(this.players[1].getChildByName("playerLabel").getComponent(cc.Label).string)
+
         for (let i = 0; i < GameData.totolUser.length; i++) {
             if(GameData.totolUser[i].userID !=GameData.userID){
                 this.players[i].getChildByName("AnimNode").getChildByName("playerLabel").getComponent(cc.Label).string = GameData.totolUser[i].userID;
@@ -52,45 +81,41 @@ cc.Class({
         }
 
         var _that = this;
-        let myScorce = {userID:GameData.userID,Score:this.score};
+        this.myScore = {userID:GameData.userID,Score:this.score};
 
-        this.userScores.push(myScorce);
-        this.scoreDisplays = [this.scoreDisplays0,this.scoreDisplays1];
+        // this.userScores.push(myScore);
+        // this.scoreDisplays = [this.scoreDisplays0,this.scoreDisplays1];
         // console.log(this.userScore);
+        //////////喂食
         this.giveFood.on(cc.Node.EventType.TOUCH_END,function(event){
 
             for (let i = 0; i < _that.players.length; i++) {
-                // (!_that.players[i]) && (_that.players[i] = _that.node.getChildByName("Player" + (i + 1)).node);
                 var lableId = _that.players[i].getChildByName("AnimNode").getChildByName("playerLabel").getComponent(cc.Label).string;
                 console.log(lableId)
                 //  console.log(GameData.userID)
                 if(lableId == "我"){
-                    // console.log('店家')
-                    // console.log(_that.scoreDisplays0.string)
-                    _that.userScores[i].Score++;
-                    _that.scoreDisplays[i].string = _that.userScores[i].Score ;
-                }
+                    _that.myScore.Score ++ ;
+                    _that.players[i].getChildByName("AnimNode").getChildByName("dogLevel").getComponent(cc.Label).string = _that.myScore.Score;
+                    _that.sendEvent(_that.myScore);
 
-                _that.sendEvent(_that.userScores[i]);
+                }
 
             }
         })
 
-        // this.moveObj(this.players[1])
-        for (let i = 0; i < this.players.length; i++) {
-            var lableId = this.players[i].getChildByName("AnimNode").getChildByName("playerLabel").getComponent(cc.Label).string;
-            console.log(lableId)
-            //  console.log(GameData.userID)
-            if(lableId == "我"){
-                // console.log(this.players[i])
-                this.moveObj(this.players[i])
-            }
-
-        }
 
 
 
     },
+
+    //实例化预制件，设置父节点
+    loadPrefab() {
+        let node = cc.instantiate(this.player_prefab);
+        //当父节点不存在时，使用当前组件为父节点
+        node.parent = this.parent || this.node;
+        // console.log(node.parent);
+    },
+
     initMatchvsEvent(self) {
         //在应用开始时手动绑定一下所有的回调事件
         response.prototype.bind();
@@ -107,62 +132,6 @@ cc.Class({
         this.node.off(msg.MATCHVS_SEND_EVENT_NOTIFY, this.sendEventNotify, this);
     },
 
-    //触摸物体移动
-    moveObj:function(nameStr){
-        console.log(nameStr)
-        let nameAni = nameStr.getChildByName("AnimNode")
-
-        this.node.on('touchstart', function (event) {
-            var pos = event.getLocation();
-            pos = this.node.convertToNodeSpaceAR(pos);
-            var start = nameAni.getPosition();
-            var end = pos;
-            cc.log(start);
-            cc.log(pos);
-            // dogLevel.setPosition(pos);
-            // var ros = this.getAngle(start,end);
-            // nameAni.rotation = ros;
-            // cc.log(nameAni.rotation);
-            var index = this.getDirection(start,end);
-            console.log(index);
-            // var animCtrl = nameStr.getComponent(cc.Animation);
-            var animCtrl = nameStr.getChildByName("AnimNode").getComponent(cc.Animation)
-
-            var clips = animCtrl.getClips();
-            this.sendPos = {
-                "endPosition":end,
-                "index":index,
-                "userID":GameData.userID,
-            }
-            this.sendEvent(this.sendPos);
-
-            if(animCtrl.currentClip == null || animCtrl.currentClip.name != clips[index].name ){
-                animCtrl.play(clips[index].name);
-            }
-            ////y 轴超过某个区域后不移动
-            if(end.y > -180 && end.y < 140){
-                this.moveToPoint(end,animCtrl);
-            }
-
-            // for(let i=0;i<scatterFood.length;i++){
-            //     let scX = scatterFood[i].x;
-            //     let scY = scatterFood[i].y;
-            //     if(pos.x>scX -20 && pos.x <scX +20 && pos.y > scY -20 && pos.y < scY+20){
-            //         var _that = this;
-            //         var dis = this.getDistance(start , pos);
-            //         var t = dis / 100 +1;
-            //         setTimeout(()=>{
-            //             cc.log('碰撞')
-            //             _that.giveFood();
-            //             effect1.active = true;
-            //             this.m_effect.play('effect1');
-            //             this.m_effect.node.setPosition(cc.v2(pos));
-            //         },t*1000)
-            //     }
-            // }
-
-        }, this);
-    },
     ///同步移动
     synMove:function(param){
         let getparam = JSON.parse(param);
@@ -180,25 +149,14 @@ cc.Class({
                 if(animCtrl.currentClip == null || animCtrl.currentClip.name != clips[index].name ){
                     animCtrl.play(clips[index].name);
                 }
-                this.moveToPoint(getparam.endPosition,animCtrl);
+                if(getparam.endPosition.y > -180 && getparam.endPosition.y < 140){
+                    this.players[i].getComponent("Player").moveToPoint(getparam.endPosition,animCtrl);
+                }
+                // this.moveToPoint(getparam.endPosition,animCtrl);
             }
         }
-        console.log(getparam)
     },
-    moveToPoint:function(point,nameStr){
-        console.log(point);
-        console.log(nameStr);
-        var start = nameStr.node.getPosition();
-        if(this.m_moveToAction != null){
-            nameStr.node.stopAction(this.m_moveToAction)
-        }
-        //匀速 v=s/t,t=s/v
-        var dis = this.getDistance(start , point);
-        var t = dis / 100;
-        this.m_moveToAction = cc.moveTo(t, point);
-        // 执行动作
-        nameStr.node.runAction(this.m_moveToAction);
-    },
+
     /**
      * 发送信息
      */
@@ -214,9 +172,6 @@ cc.Class({
             }
 
         }
-
-        // this.labelLog('你准备使出一招：' + 0);
-        // this.engineCode(result, 'sendEvent');
     },
     /**
      * 发送消息回调
@@ -237,12 +192,10 @@ cc.Class({
     sendEventNotify(eventInfo) {
         let getPosition = JSON.parse(eventInfo.cpProto);
         if(getPosition.endPosition){
-            console.log("000000")
             this.synMove(eventInfo.cpProto);
         }else{
             this.refreshScore(eventInfo)
         }
-        console.log(eventInfo);
 
         console.log('sendEventNotify：用户' + eventInfo.srcUserID + '对你使出了一招' + eventInfo.cpProto);
     },
@@ -254,18 +207,13 @@ cc.Class({
         let selfLevel = parseInt(event.cpProto);
         if (event !== undefined) {
             if(typeof selfLevel == "number"){
-                this.scoreDisplays = [this.scoreDisplays0,this.scoreDisplays1];
                 for (let i = 0; i < this.players.length; i++) {
-                    // (!_that.players[i]) && (_that.players[i] = _that.node.getChildByName("Player" + (i + 1)).node);
                     var lableId = this.players[i].getChildByName("AnimNode").getChildByName("playerLabel").getComponent(cc.Label).string;
                     console.log(lableId)
                     //  console.log(GameData.userID)
                     if(lableId == event.srcUserID){
-                        console.log('店家')
-                        console.log(this.scoreDisplays0.string)
-                        this.scoreDisplays[i].string = selfLevel;
+                        this.players[i].getChildByName("AnimNode").getChildByName("dogLevel").getComponent(cc.Label).string = selfLevel;
                     }
-
 
                 }
             }
@@ -274,41 +222,5 @@ cc.Class({
         }
     },
 
-    getDistance:function(start,end){
-        var pos = cc.v2(start.x - end.x , start.y - end.y);
-        var dis = Math.sqrt(pos.x*pos.x + pos.y * pos.y);
-        return dis;
-    },
-    getDirection:function(start,end){
-        var rot = this.getAngle(start,end);
-        console.log(rot);
-        if(rot > -45 && rot < 45){
-            return 0;
-        }else if( rot > 45 && rot < 135){
-            return 1;
-        }else if( rot > 135 && rot < 225){
-            return 2;
-        }else {
-            return 3;
-        }
-    },
-    getAngle:function(start,end){
-        //两点的x,y值
-        var x = end.x - start.x;
-        var y = end.y - start.y;
-        var hypotenuse = Math.sqrt(x * x + y * y);
-        //斜边长度
-        var cos = x / hypotenuse;
-        var radian = Math.acos(cos);
-        //求出弧度
-        var angle = 180 /(Math.PI / radian);
-        //用弧度计算角度
-        if( y < 0){
-            angle = 0-angle;
-        }else if( y == 0 && x < 0){
-            angle = 180;
-        }
-        return 90 - angle;
-    },
 
 });
--- assets/scripts/interface/Match.js
+++ assets/scripts/interface/Match.js
@@ -64,7 +64,7 @@ cc.Class({
         // console.log("随机匹配result"+result);
         // mvs.response.joinRoomResponse = (status, roomUserInfoList, roomInfo)=>{
         //     console.log(status);
-            
+
         //     console.log(roomUserInfoList);
         //     console.log(roomInfo);
         //     this.userInfo.push(GameData.userInfo);
@@ -93,16 +93,16 @@ cc.Class({
         //             }
         //             var result = mvs.engine.sendEvent('你使出一招:' + '19090909');
         //             console.log(result)
-            
+
         //         }
         //         //成功
         //     }else{
         //         //失败
         //     }
         // }
-       
 
-        
+
+
 
 
     },
@@ -114,38 +114,16 @@ cc.Class({
         //在应用开始时手动绑定一下所有的回调事件
         response.prototype.bind();
         response.prototype.init(self);
-        // this.node.on(msg.MATCHVS_INIT, this.initResponse, this);
-        // this.node.on(msg.MATCHVS_REGISTER_USER, this.registerUserResponse, this);
-        // this.node.on(msg.MATCHVS_LOGIN, this.loginResponse, this);
         this.node.on(msg.MATCHVS_JOIN_ROOM_RSP, this.joinRoomResponse, this);
         this.node.on(msg.MATCHVS_JOIN_ROOM_NOTIFY, this.joinRoomNotify, this);
-        // this.node.on(msg.MATCHVS_JOIN_OVER_RSP, this.joinOverResponse, this);
-        // this.node.on(msg.MATCHVS_JOIN_OVER_NOTIFY, this.joinOverNotify, this);
-        this.node.on(msg.MATCHVS_SEND_EVENT_RSP, this.sendEventResponse, this);
-        this.node.on(msg.MATCHVS_SEND_EVENT_NOTIFY, this.sendEventNotify, this);
-        // this.node.on(msg.MATCHVS_LEAVE_ROOM, this.leaveRoomResponse, this);
-        // this.node.on(msg.MATCHVS_LEAVE_ROOM_NOTIFY, this.leaveRoomNotify, this);
-        // this.node.on(msg.MATCHVS_LOGOUT, this.logoutResponse, this);
-        // this.node.on(msg.MATCHVS_ERROE_MSG, this.errorResponse, this);
     },
 
     /**
      * 移除监听
      */
     removeEvent() {
-        // this.node.off(msg.MATCHVS_INIT, this.initResponse, this);
-        // this.node.off(msg.MATCHVS_REGISTER_USER, this.registerUserResponse, this);
-        // this.node.off(msg.MATCHVS_LOGIN, this.loginResponse, this);
         this.node.off(msg.MATCHVS_JOIN_ROOM_RSP, this.joinRoomResponse, this);
         this.node.off(msg.MATCHVS_JOIN_ROOM_NOTIFY, this.joinRoomNotify, this);
-        // this.node.off(msg.MATCHVS_JOIN_OVER_RSP, this.joinOverResponse, this);
-        // this.node.off(msg.MATCHVS_JOIN_OVER_NOTIFY, this.joinOverNotify, this);
-        this.node.off(msg.MATCHVS_SEND_EVENT_RSP, this.sendEventResponse, this);
-        this.node.off(msg.MATCHVS_SEND_EVENT_NOTIFY, this.sendEventNotify, this);
-        // this.node.off(msg.MATCHVS_LEAVE_ROOM, this.leaveRoomResponse, this);
-        // this.node.off(msg.MATCHVS_LEAVE_ROOM_NOTIFY, this.leaveRoomNotify, this);
-        // this.node.off(msg.MATCHVS_LOGOUT, this.logoutResponse, this);
-        // this.node.off(msg.MATCHVS_ERROE_MSG, this.errorResponse, this);
     },
     /**
      * 进入房间回调
@@ -159,7 +137,7 @@ cc.Class({
         console.log(roomInfo)
         if (status == 200) {
             this.labelRoomID.string = "房间ID为:"+roomInfo.roomID;
-            
+
             console.log('joinRoomResponse: 进入房间成功：房间ID为：' + roomInfo.roomID + '房主ID：' + roomInfo.ownerId + '房间属性为：' + roomInfo.roomProperty);
             for (var i = 0; i < userInfoList.length; i++) {
                 console.log('joinRoomResponse：房间的玩家ID是' + userInfoList[i].userID);
@@ -168,13 +146,10 @@ cc.Class({
                 console.log('joinRoomResponse：房间暂时无其他玩家');
             }
             if(userInfoList.length!=0){
-                console.log(JSON.parse(userInfoList[0].userProfile))
                 this.totolUser.push(JSON.parse(userInfoList[0].userProfile))
-                this.playerNameTwo.string = userInfoList[0].userID;
+                this.playerNameTwo.string = JSON.parse(userInfoList[0].userProfile).name;
                 GameData.totolUser = this.totolUser;
                 this.startGame()
-                // this.sendEvent(this.totolUser);
-                this.getUserInfo(userInfoList[0].userProfile);
             }
         } else {
             console.log('joinRoomResponse：进入房间失败');
@@ -185,60 +160,14 @@ cc.Class({
      * @param roomUserInfo
      */
     joinRoomNotify(roomUserInfo) {
-        console.log(roomUserInfo);
+        this.playerNameTwo.string = JSON.parse(roomUserInfo.userProfile).name;
+        this.totolUser.push(JSON.parse(roomUserInfo.userProfile))
+        GameData.totolUser = this.totolUser;
+        this.startGame();
         console.log('joinRoomNotify：加入房间的玩家ID是' + roomUserInfo.userID);
-    },
-    
-    getUserInfo(userList){
-        console.log(userList)
-        engine.prototype.sendEvent(userList);
-        // console.log(msg)
-    },
-    /**
-     * 发送信息
-     */
-    sendEvent() {
-        var eventMsg = ['万剑归宗', ' 亢龙有悔', '庐山升龙霸 ', ' 天马流行拳', ' 钻石星尘', ' 凤翼天翔',
-            '庐山亢龙霸 ', '极冻冰棺', ' 等离子光速拳', '星云锁链'];
-        var msg = eventMsg[Math.floor(Math.random() * 10)];
-        var result = engine.prototype.sendEvent('你使出一招:' + msg);
-        console.log(msg)
-        console.log('你准备使出一招：' + msg);
-        // this.engineCode(result, 'sendEvent');
-    },
-    /**
-     * 发送消息回调
-     * @param sendEventRsp
-     */
-    sendEventResponse(sendEventRsp) {
-        if (sendEventRsp.status == 200) {
-            console.log('sendEventResponse：发送消息成功');
-        } else {
-            console.log('sendEventResponse：发送消息失败');
-        }
-    },
-
-    /**
-     * 接收到其他用户消息通知
-     * @param eventInfo
-     */
-    sendEventNotify(eventInfo) {
-        console.log(eventInfo);
-        
-        if(eventInfo!=undefined){
-            var userINFO = {
-                userID:eventInfo.srcUserID
-            }
-            GameData.totolUser.push(GameData.userInfo)
-            GameData.totolUser.push(userINFO)
-            this.playerNameTwo.string = eventInfo.srcUserID;
-            this.startGame()
-        }
-        console.log('sendEventNotify：用户' + eventInfo.srcUserID + '对你使出了一招' + eventInfo.cpProto);
     },
 
     startGame: function () {
         cc.director.loadScene('Game');
-        console.log(this.totolUser)
     },
 });
